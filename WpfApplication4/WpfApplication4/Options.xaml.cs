ee77ccca71b997d84640171a1d5de5adbb84e137
Nathan Lyon <natelyon8@gmail.com>
2013-04-11 19:18:50 -0600

    ...

--- WpfApplication4/WpfApplication4/Options.xaml.cs
+++ WpfApplication4/WpfApplication4/Options.xaml.cs
@@ -9,18 +9,71 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfApplication4
 {
     /// <summary>
-    /// Interaction logic for Options.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class Options : Window
     {
+         int decks = 0;
+         bool softdeal = false;
+         string tie;
+
         public Options()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("decks = "+decks+"\nsoftdeal = "+ softdeal+"\ntie = "+tie );
+        }
+
+        private void radioButton1_Checked_1(object sender, RoutedEventArgs e)
+        {
+            tie = "dealer";
+        }
+
+        private void radioButton2_Checked(object sender, RoutedEventArgs e)
+        {
+            tie = "player";
+        }
+
+        private void radioButton3_Checked(object sender, RoutedEventArgs e)
+        {
+            tie = "push";
+        }
+
+        private void radioButton4_Checked(object sender, RoutedEventArgs e)
+        {
+            softdeal = true;
+        }
+
+        private void radioButton5_Checked(object sender, RoutedEventArgs e)
+        {
+            softdeal = false;
+        }
+
+        private void radioButton6_Checked(object sender, RoutedEventArgs e)
+        {
+            decks = 1;
+        }
+
+        private void radioButton7_Checked(object sender, RoutedEventArgs e)
+        {
+            decks = 2;
+        }
+
+        private void radioButton8_Checked(object sender, RoutedEventArgs e)
+        {
+            decks = 5;
+        }
+
+       
+
     }
 }
