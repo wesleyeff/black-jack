f1994c1525e14788f7c5d048efd67c5d7d1d8626
Steve Young <stvnyoung@gmail.com>
2013-04-25 18:24:34 -0600

    Updated rules

--- Blackjack/Blackjack/Properties/AssemblyInfo.cs
+++ Blackjack/Blackjack/Properties/AssemblyInfo.cs
@@ -7,13 +7,13 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WpfApplication4")]
+[assembly: AssemblyTitle("Blackjack")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WpfApplication4")]
+[assembly: AssemblyCompany("Team Discovery Channel")]
+[assembly: AssemblyProduct("CS2450 Black Jack")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("TeamDiscoveryChannel")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
